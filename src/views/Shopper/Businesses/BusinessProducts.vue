c4d97df48826e4dc78803b55ce8ea984353acd60
neema491 <57594371+neema491@users.noreply.github.com>
2023-10-20 21:20:38 +0300

    Update BusinessProducts.vue

--- src/views/Shopper/Businesses/BusinessProducts.vue
+++ src/views/Shopper/Businesses/BusinessProducts.vue
@@ -1,39 +1,3 @@
-<template>
-  <ion-page>
-
-    <section class="ion-padding">
-      <IonHeader class="inner-header">
-        <IonToolbar class="ion-align-items-center">
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/shopper/home/businesses"></IonBackButton>
-          </IonButtons>
-
-          <IonTitle size="small" class="fw-bold">
-            {{ business?.name }} Products
-          </IonTitle>
-
-          <IonButtons slot="end">
-            <NotificationButton></NotificationButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-    </section>
-
-    <ion-content :fullscreen="true">
-      <section v-if="fetching" class="ion-text-center d-flex ion-justify-content-center ion-padding">
-        <IonSpinner name="crescent"></IonSpinner>
-      </section>
-      <IonGrid v-if="!fetching">
-        <IonRow>
-          <IonCol size="6" v-for="product in products" :key="product.id">
-            <ProductCard :product="product"></ProductCard>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </ion-content>
-  </ion-page>
-</template>
-
 <script setup lang="ts">
 import { IonPage, IonContent, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonSpinner, IonGrid, IonCol, IonRow } from '@ionic/vue';
 import NotificationButton from '@/components/notifications/NotificationButton.vue';
@@ -55,7 +19,7 @@ const businessId = route.params.id;
 onMounted(async () => {
   fetching.value = true;
   business.value = await businessStore.getBusiness(+route.params.id);
-  products.value = await businessStore.getBusinessProducts(Business.value);
+  products.value = await businessStore.getBusinessProducts(business.value); // Use business.value here
   fetching.value = false;
 });
 </script>
