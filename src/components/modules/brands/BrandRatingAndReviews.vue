b14099081c132cb7b55a963740446a19c29ae5c5
hagan199 <haganemmanuel23@gmail.com>
2023-10-20 22:38:23 +0000

    Change review text to blue on Brand RatingAnd Review

--- src/components/modules/brands/BrandRatingAndReviews.vue
+++ src/components/modules/brands/BrandRatingAndReviews.vue
@@ -1,54 +1,53 @@
 <template>
-  <section class="rating-and-reviews">
-      <section class="rating">
-          <span>{{  1 }}</span>
-          <span>⭐</span>
-          <span class="reviews" v-if="showReviews">{{ 0 }} Reviews</span>
-      </section>
-    
-  </section>
+    <section class="rating-and-reviews">
+        <section class="rating">
+            <span>{{ 1 }}</span>
+            <span>⭐</span>
+            <span class="reviews larger-text " v-if="showReviews">{{ business?.reviews_count || 0 }}  Reviews</span>
+        </section>
+
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
-import Brand from '@/models/Brand';
+
+import Business from '@/models/Business';
 import { locationOutline } from 'ionicons/icons';
 import { IonIcon, IonText } from '@ionic/vue';
 
 export default defineComponent({
-  props: {
-      brand: {
-          type: Object as PropType<Brand | null> 
-      },
-      showReviews: {
-          type: Boolean,
-          default: true
-      }
-  },
-  data() {
-      return {
-          locationOutline
-      };
-  },
-  components: { IonIcon, IonText }
+    props: {
+        business: {
+            type: Object as PropType<Business | null>
+        },
+        showReviews: {
+            type: Boolean,
+            default: true
+        }
+    },
+    data() {
+        return {
+            locationOutline
+        };
+    },
+    components: { IonIcon, IonText, Business }
 })
 </script>
 
-
 <style scoped lang="scss">
-  .rating-and-reviews {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      font-size: 0.85em;
-      padding-left: 10;
+.rating-and-reviews {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
 
-      .reviews {
-          margin-left: 9px;
-      }
+.larger-text {
+    font-size: 1.0em;
+    color: #666EED;
+}
 
-      .distance {
-          color: #666EED;
-      }
-  }
+.distance {
+    color: #666EED;
+}
 </style>
