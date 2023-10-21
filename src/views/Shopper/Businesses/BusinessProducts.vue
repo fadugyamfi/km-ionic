f68b75d06f17a7ba2531e67a80a15b170ca2a09a
neema491 <57594371+neema491@users.noreply.github.com>
2023-10-21 07:31:58 +0300

    Update BusinessProducts.vue

--- src/views/Shopper/Businesses/BusinessProducts.vue
+++ src/views/Shopper/Businesses/BusinessProducts.vue
@@ -1,3 +1,34 @@
+<template>
+  <ion-page>
+    <section class="ion-padding">
+      <IonHeader class="inner-header">
+        <IonToolbar class="ion-align-items-center">
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/shopper/home/categories"></IonBackButton>
+          </IonButtons>
+          <IonTitle size="small" class="fw-bold">{{ business?.name }}</IonTitle>
+          <IonButtons slot="end">
+            <NotificationButton></NotificationButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+    </section>
+
+    <ion-content :fullscreen="true">
+      <section v-if="fetching" class="ion-text-center d-flex ion-justify-content-center ion-padding">
+        <IonSpinner name="crescent"></IonSpinner>
+      </section>
+      <IonGrid v-if="!fetching">
+        <IonRow>
+          <IonCol size="6" v-for="product in products" :key="product.id">
+            <ProductCard :product="product"></ProductCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </ion-content>
+  </ion-page>
+</template>
+
 <script setup lang="ts">
 import { IonPage, IonContent, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonSpinner, IonGrid, IonCol, IonRow } from '@ionic/vue';
 import NotificationButton from '@/components/notifications/NotificationButton.vue';
@@ -10,16 +41,14 @@ import ProductCard from '@/components/cards/ProductCard.vue';
 
 const businessStore = useBusinessStore();
 const route = useRoute();
-const business = ref<Business|null>();
+const business = ref<Business | null>();
 const products = ref<Product[]>([]);
 const fetching = ref(false);
-
-const businessId = route.params.id;
 
 onMounted(async () => {
   fetching.value = true;
   business.value = await businessStore.getBusiness(+route.params.id);
-  products.value = await businessStore.getBusinessProducts(business.value); // Use business.value here
+  products.value = await businessStore.getBusinessProducts(business.value);
   fetching.value = false;
 });
 </script>
