d9715ba288f80b34cd79614b67e0460e0bf4431c
hagan199 <haganemmanuel23@gmail.com>
2023-10-24 21:51:29 +0000

    update on Cart

---
+++ src/components/cards/CartCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <ion-card class="banner">
+    <ion-row class="banner-content">
+      <ion-col size="12" size-md="6">
+        <ion-card class="cart-item">
+          <ion-card-content>
+            <ion-row class="item-info">
+              <ion-col>
+                <h2 class="product-name">Cart</h2>
+              </ion-col>
+              <ion-col class="quantity-badge">
+                <ion-badge color="warning">2</ion-badge>
+              </ion-col>
+            </ion-row>
+          </ion-card-content>
+        </ion-card>
+      </ion-col>
+      <ion-col size="12" size-md="6">
+        <ion-card class="saved-label">
+          <ion-card-content>
+            <h2>Saved</h2>
+          </ion-card-content>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-card>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent",
+};
+</script>
+
+<style scoped lang="scss">
+.banner {
+  width: 358px;
+  height: 46px;
+  padding: 4px;
+  background: rgba(167, 167, 167, 0.20);
+  border-radius: 8px;
+  border: 1px #F2F4F7 solid;
+  .banner-content {
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    display: inline-flex;
+    .cart-item,
+    .saved-label {
+      flex: 1 1 0;
+      height: 38px;
+      padding-left: 12px;
+      padding-right: 12px;
+      padding-top: 8px;
+      padding-bottom: 8px;
+      background: white;
+      box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.06);
+      border-radius: 6px;
+      overflow: hidden;
+      justify-content: center;
+      align-items: center;
+      gap: 8px;
+      display: flex;
+      .item-info {
+        .product-name {
+          color: #344054;
+          font-size: 14px;
+          font-family: Poppins;
+          font-weight: 500;
+          line-height: 20px;
+          word-wrap: break-word;
+        }
+        .quantity-badge {
+          ion-badge {
+            padding-left: 8px;
+            padding-right: 8px;
+            padding-top: 2px;
+            padding-bottom: 2px;
+            background: rgba(245, 170, 41, 0.38);
+            border-radius: 16px;
+            justify-content: center;
+            align-items: center;
+          }
+        }
+      }
+      .saved-label {
+        color: #667085;
+        font-size: 14px;
+        font-family: Poppins;
+        font-weight: 500;
+        line-height: 20px;
+        word-wrap: break-word;
+      }
+    }
+  }
+}
+</style>
--- src/views/Shopper/Cart.vue
+++ src/views/Shopper/Cart.vue
@@ -1,38 +1,222 @@
 <template>
   <ion-page>
     <ShopperHeader></ShopperHeader>
-
+    <section class="ion-padding">
+      <IonHeader class="inner-header">
+        <IonToolbar class="ion-align-items-center">
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/shopper/home"></IonBackButton>
+          </IonButtons>
+          <IonTitle size="small" class="fw-bold">
+            Cart
+          </IonTitle>
+          <IonButtons slot="end">
+            <NotificationButton></NotificationButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+    </section>
     <ion-content :fullscreen="true" class="ion-padding-horizontal">
-      <h4>Your Cart</h4>
+
+      <CartCard></CartCard>
+
+      <IonSegment value="personal" mode="ios" v-model="viewing">
+        <IonSegmentButton value="cart">
+          <IonLabel :class="{'yellow-circle': segmentValue === 'cart'}">
+  Cart
+  <ion-text class="badge-text">{{ totalItems }}</ion-text>
+</IonLabel>
+</IonSegmentButton>
+        <IonSegmentButton value="saved">
+          <ion-label>Saved</ion-label>
+        </IonSegmentButton>
+      </IonSegment>
+
       <IonList>
         <IonItem v-for="(item, index) in cartStore.items" :key="item.product?.id">
-          <IonAvatar slot="start">
+          <IonAvatar slot="start" class="fullcover">
             <IonImg :src="item.product?.image"></IonImg>
           </IonAvatar>
           <IonLabel>
-            <p>{{ item.product.product_name }}</p>
-            <p>{{ item.quantity }}</p>
+            <p class="text-product">{{ item.product.product_name }}</p>
+            <p>Quantity: &nbsp;{{ item.quantity }}</p> 
             <p>{{ item.product.currency?.symbol || 'GHS' }} {{ item.quantity * (item.product.product_price || 0) }}</p>
+            <ProductQuantitySelector @change="updateQuantity(item, $event)"></ProductQuantitySelector>
           </IonLabel>
-          <IonButton fill="clear" color="danger" slot="end" @click.prevent.stop="removeFromCart(item, index)">
-            <IonIcon :icon="trashBinOutline"></IonIcon>
+          <IonButton fill="clear" color="black" slot="end" @click.prevent.stop="removeFromCart(item, index)">
+            <IonIcon :icon="closeCircleOutline"></IonIcon>
           </IonButton>
         </IonItem>
       </IonList>
+
+      <ion-card class="custom-card">
+        <ion-row class="row">
+          <ion-col class="col">
+            <ion-row class="inner-row">
+              <ion-col size="auto">
+                <ion-text class="item-text">Item total</ion-text>
+              </ion-col>
+              <ion-col size="auto">
+                <ion-text class="item-price">{{ 0 }}</ion-text>
+              </ion-col>
+            </ion-row>
+          </ion-col>
+        </ion-row>
+        <ion-row class="divider-row">
+          <ion-col class="divider-col"></ion-col>
+        </ion-row>
+        <ion-row class="row">
+          <ion-col class="col">
+            <ion-row class="inner-row">
+              <ion-col size="auto">
+                <ion-text class="item-text">Delivery</ion-text>
+              </ion-col>
+              <ion-col size="auto">
+                <ion-text class="item-price">TBD</ion-text>
+              </ion-col>
+            </ion-row>
+          </ion-col>
+        </ion-row>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonImg, IonButton, IonIcon } from '@ionic/vue';
-import ShopperHeader from '@/components/layout/ShopperHeader.vue';
+
+import {
+  IonPage, IonContent, IonToolbar,
+  IonButtons, IonBackButton, IonHeader,
+  IonSegment, IonSegmentButton,
+  IonTitle, IonList, IonItem, IonLabel,
+  IonCard, IonRow, IonCol, IonText,
+  IonAvatar, IonImg, IonButton, IonIcon
+} from '@ionic/vue';
 import { CartItem, useCartStore } from '@/stores/CartStore';
-import { trashBinOutline } from 'ionicons/icons';
+import ShopperHeader from '@/components/layout/ShopperHeader.vue';
+import ProductQuantitySelector from '@/components/modules/products/ProductQuantitySelector.vue';
+import { closeCircleOutline } from 'ionicons/icons';
+import { ref, computed } from 'vue';
 
 const cartStore = useCartStore();
 cartStore.loadFromStorage();
+const viewing = ref('cart');
+
+const calculateTotalCost = computed(() => {
+  let totalCost = 0;
+  for (const item of cartStore.items) {
+    totalCost += item.quantity * (item.product.product_price || 0);
+  }
+  return `GHS ${totalCost.toFixed(2)}`;
+});
+
+const segmentValue = ref('cart'); 
+const updateQuantity = (item: CartItem, newQuantity: number) => {
+  item.quantity = newQuantity;
+
+}
+const totalItems = computed(() => {
+  return 0;
+});
 
 const removeFromCart = (item: CartItem, index: number) => {
   cartStore.removeAtIndex(index);
 }
 </script>
+
+
+<style scoped lang="scss">
+.fullcover {
+  width: 94px;
+  height: 120px;
+  border-radius: 12px;
+  background-size: cover;
+  border: 2px solid rgb(243, 237, 237);
+  border-radius: 10px;
+}
+
+.custom-card {
+  height: 85px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  background: white;
+  box-shadow: 0px 4px 24px rgba(105, 111, 130, 0.10);
+  border-radius: 8px;
+}
+
+.text-product {
+    color: black;
+  }
+
+.row {
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: inline-flex;
+}
+
+.inner-row {
+  width: 326.17px;
+  justify-content: flex-start;
+  align-items: flex-end;
+  gap: 88px;
+  display: inline-flex;
+}
+
+.col {
+  flex: 1 1 0;
+  height: 22px;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 93px;
+  display: flex;
+}
+
+.item-text {
+  color: black;
+  font-size: 14px;
+  font-family: "Poppins";
+  font-weight: 400;
+  text-transform: capitalize;
+  line-height: 22px;
+  word-wrap: break-word;
+}
+
+.badge-text {
+  background: yellow;
+  border-radius: 50%;
+  padding: 4px;
+  display: inline-block;
+  color: black; 
+  font-size: 14px;
+  font-family: Poppins;
+  font-weight: 500;
+  line-height: 20px;
+  word-wrap: break-word;
+}
+
+.item-price {
+  color: black;
+  font-size: 14px;
+  font-family: "Poppins";
+  font-weight: 400;
+  text-transform: capitalize;
+  line-height: 18px;
+  word-wrap: break-word;
+  row-gap: 49px;
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+
+.divider-row {
+  width: 326px;
+  height: 0px;
+  border: 1px #F1F1F1 solid;
+}
+
+.divider-col {
+  height: 100%;
+}
+</style>
