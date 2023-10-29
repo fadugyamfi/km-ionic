178a53c327caf404bb3f42a217d5d3da41313e5d
hagan199 <haganemmanuel23@gmail.com>
2023-10-29 22:14:46 +0000

    business product images

--- src/components/modules/carts/OrderView.vue
+++ src/components/modules/carts/OrderView.vue
@@ -1,112 +1,133 @@
 <template>
-    <IonItem>
+  <IonItem>
+    <section class="d-flex ion-align-items-stretch">
+      <IonThumbnail>
+        <Image :src="order?.business?.logo"></Image>
+      </IonThumbnail>
 
-
-      <section class="d-flex ion-align-items-stretch">
-        <IonThumbnail>
-          <Image :src="order?.business?.logo"></Image>
-        </IonThumbnail>
-
-        <section>
-          <section class="d-flex ion-justify-content-between">
-
-            <section class="d-flex flex-column">
-              <IonText class="fw-semibold">{{ order?.business?.name || 'Unknown' }}</IonText>
-              <IonText color="medium" class="font-medium">
-                {{ $t("Item total") }}: {{ order?.getTotal().toLocaleString('en-GB', { style: 'currency', currency: 'GHS' }) }}
-              </IonText>
-              <IonText color="medium" class="font-medium">
-                GHS 3000 minimum reached
-              </IonText>
-
+      <section>
+        <section
+          style="height: 100%"
+          class="d-flex ion-justify-content-between"
+        >
+          <section class="d-flex flex-column">
+            <IonText class="fw-semibold">{{
+              order?.business?.name || "Unknown"
+            }}</IonText>
+            <IonText color="medium" class="font-medium">
+              {{ $t("Item total") }}:
+              {{
+                order?.getTotal().toLocaleString("en-GB", {
+                  style: "currency",
+                  currency: "GHS",
+                })
+              }}
+            </IonText>
+            <IonText color="medium" class="font-medium">
+              GHS 3000 minimum reached
+            </IonText>
+            <section>
+              {{ order.order_items }}
+              <!-- <IonThumbnail  class="cart-items">
+                <Image :src=""></Image>
+              </IonThumbnail> -->
             </section>
-
-            <IonButton slot="end" fill="clear" color="dark" class="ion-no-margin ion-no-padding ion-align-self-start"
-              @click="removeOrder()">
-              <IonIcon slot="icon-only" :icon="closeCircleOutline"></IonIcon>
-            </IonButton>
           </section>
 
+          <IonButton
+            slot="end"
+            fill="clear"
+            color="dark"
+            class="ion-no-margin ion-no-padding ion-align-self-start"
+            @click="removeOrder()"
+          >
+            <IonIcon slot="icon-only" :icon="closeCircleOutline"></IonIcon>
+          </IonButton>
         </section>
       </section>
+    </section>
+  </IonItem>
+</template>
 
-    </IonItem>
-  </template>
+<script lang="ts">
+import { IonButton, IonIcon, IonItem, IonText, IonThumbnail } from "@ionic/vue";
+import { PropType, defineComponent } from "vue";
+import ProductQuantitySelector from "../products/ProductQuantitySelector.vue";
+import { closeCircleOutline } from "ionicons/icons";
+import Image from "@/components/Image.vue";
+import { mapStores } from "pinia";
+import { useSaleStore } from "@/stores/SaleStore";
+import { useCartStore } from "@/stores/CartStore";
+import Product from "@/models/Product";
+import { Order } from "@/models/Order";
 
-  <script lang="ts">
-  import { IonButton, IonIcon, IonItem, IonText, IonThumbnail } from '@ionic/vue';
-  import { PropType, defineComponent } from 'vue';
-  import ProductQuantitySelector from '../products/ProductQuantitySelector.vue';
-  import { closeCircleOutline } from 'ionicons/icons';
-  import Image from '@/components/Image.vue';
-  import { mapStores } from 'pinia';
-  import { useSaleStore } from '@/stores/SaleStore';
-  import { useCartStore } from '@/stores/CartStore';
-  import Product from '@/models/Product';
-  import { Order } from '@/models/Order';
+export default defineComponent({
+  components: {
+    IonItem,
+    IonThumbnail,
+    IonText,
+    ProductQuantitySelector,
+    IonButton,
+    IonIcon,
+    Image,
+  },
 
+  props: {
+    order: {
+      type: Object as PropType<Order>,
+    },
+  },
 
-  export default defineComponent({
+  data() {
+    return {
+      closeCircleOutline,
+    };
+  },
 
-    components: {
-      IonItem,
-      IonThumbnail,
-      IonText,
-      ProductQuantitySelector,
-      IonButton,
-      IonIcon,
-      Image
+  computed: {
+    ...mapStores(useSaleStore, useCartStore),
+  },
+
+  methods: {
+    updateItemQuantity(quantity: number) {
+      this.order?.update({
+        quantity,
+        total_price: quantity * (this.order?.product?.product_price || 0),
+      });
     },
 
-    props: {
-      order: {
-        type: Object as PropType<Order>
-      }
+    removeOrder() {
+      /// this.cartStore.removeProductFromSale(this.saleItem?.product as Product);
     },
+  },
+});
+</script>
 
-    data() {
-      return {
-        closeCircleOutline
-      }
-    },
+<style scoped>
+ion-item {
+  --inner-padding-start: 0px;
+  --inner-padding-end: 0px;
+  --padding-start: 0px;
+  margin-bottom: 10px;
+}
 
-    computed: {
-      ...mapStores(useSaleStore, useCartStore )
-    },
+.custom-image {
+  width: 24px;
+  height: 24px;
+  border-radius: 2px;
+}
 
-    methods: {
-      updateItemQuantity(quantity: number) {
-        this.order?.update({
-          quantity,
-          total_price: quantity * (this.order?.product?.product_price || 0)
-        });
-      },
+ion-thumbnail {
+  min-width: 85px;
+  height: 120px;
+  margin-right: 10px;
+  --border-radius: 8px;
+}
 
-      removeOrder() {
-        /// this.cartStore.removeProductFromSale(this.saleItem?.product as Product);
-      }
-    }
-  })
-  </script>
-
-  <style scoped>
-  ion-item {
-    --inner-padding-start: 0px;
-    --inner-padding-end: 0px;
-    --padding-start: 0px;
-    margin-bottom: 10px;
-  }
-
-  .custom-image {
-    width: 24px;
-    height: 24px;
-    border-radius: 2px;
-  }
-
-  ion-thumbnail {
-    min-width: 85px;
-    height: 140px;
-    margin-right: 10px;
-    --border-radius: 8px;
-  }
-  </style>
+ion-thumbnail.cart-items {
+  margin-top: 5px;
+  min-width: 24px;
+  height: 45px;
+  --border-radius: 2px;
+}
+</style>
--- src/stores/CartStore.ts
+++ src/stores/CartStore.ts
@@ -2,188 +2,187 @@ import axios from "axios";
 import { defineStore } from "pinia";
 import Product from "@/models/Product";
 import { Order } from "@/models/Order";
-import { useToastStore } from '@/stores/ToastStore';
-import { useBusinessStore } from '@/stores/BusinessStore';
-import AppStorage from './AppStorage';
+import { useToastStore } from "@/stores/ToastStore";
+import { useBusinessStore } from "@/stores/BusinessStore";
+import AppStorage from "./AppStorage";
 import { useUserStore } from "./UserStore";
 import { OrderItem } from "../models/OrderItem";
 
 const storage = new AppStorage();
-const KOLA_CART = 'kola.cart';
+const KOLA_CART = "kola.cart";
 
-
-const businessStore = useBusinessStore()
+const businessStore = useBusinessStore();
 
 export type CartItem = {
-    product: Product;
-    quantity: number;
+  product: Product;
+  quantity: number;
 };
 
 export const useCartStore = defineStore("cart", {
+  state: () => {
+    return {
+      items: [] as CartItem[],
+      businesses: [] as any,
+      orders: [] as Order[],
+    };
+  },
 
-    state: () => {
-        return {
-            items: [] as CartItem[],
-            businesses: [] as any,
-            orders: [] as Order[]
-        }
+  actions: {
+    async loadFromStorage() {
+      let data = await storage.get(KOLA_CART);
+
+      this.orders = (data.map((d: object) => new Order(d)) || []) as Order[];
+
+      return this;
     },
 
-    actions: {
-        async loadFromStorage() {
-            let data = await storage.get(KOLA_CART);
+    getProductItem(product: Product): OrderItem | null {
+      let existingProduct: OrderItem | null = null;
 
-            this.orders = (data.map((d: object) => new Order(d)) || []) as Order[];
+      this.orders.forEach((order: Order) => {
+        const p = order.order_items.find((el) => (el.products_id = product.id));
 
-            return this;
-        },
-
-        getProductItem(product: Product): OrderItem | null {
-            let existingProduct: OrderItem | null = null;
-
-            this.orders.forEach((order: Order) => {
-                const p = order.order_items.find(el => el.products_id = product.id);
-
-                if( p ) {
-                    existingProduct = p;
-                    return false;
-                }
-            });
-
-            return existingProduct;
-        },
-
-        hasProduct(product: Product): boolean {
-            if (!product || !this.orders) {
-                return false;
-            }
-
-            let exists = false;
-
-            this.orders.forEach((order: Order) => {
-                const index = order.order_items.findIndex(el => el.products_id = product.id);
-                if( index > -1 ) {
-                    exists = true;
-                    return false;
-                }
-            });
-
-            return exists;
-        },
-
-        // addProduct(product: Product, quantity = 1) {
-        //     const toastStore = useToastStore();
-
-        //     if (this.hasProduct(product)) {
-        //         this.updateQuantity(product);
-        //         toastStore.showInfo('Increased quantity in cart');
-        //     } else {
-        //         this.items.push({ product, quantity })
-        //         toastStore.showSuccess('Added To Cart');
-        //     }
-
-        //     this.persist();
-        //     return this;
-        // },
-
-        getTotalCost() {
-            return this.orders.reduce((acc, value) => acc + value.getTotal(), 0);
-        },
-
-        addProduct(product: Product, quantity = 1) {
-            const toastStore = useToastStore();
-            const userStore = useUserStore();
-
-            let order = this.orders.find((order) => order.businesses_id == product.businesses_id);
-
-            if( !order ) {
-                order = new Order({
-                    businesses_id: product.businesses_id,
-                    customer_id: userStore.activeBusiness?.id,
-                    cms_users_id: userStore.user?.id,
-                    business: product.business
-                });
-
-                this.orders.push(order);
-            }
-
-            let orderItem = order.order_items.find((item: OrderItem) => item.products_id == product.id);
-
-            if( !orderItem ) {
-                orderItem = new OrderItem({
-                    businesses_id: product.businesses_id,
-                    products_id: product.id,
-                    unit_price: product.product_price,
-                    quantity: quantity,
-                    total_price: quantity * (product.product_price || 0),
-                    currencies_id: 1, // GHS
-                    product_units_id: 1, // GHS
-                });
-
-                order.order_items.push( orderItem );
-
-                toastStore.showSuccess('Added To Cart');
-            } else {
-                orderItem.quantity = quantity;
-                toastStore.showInfo('Increased quantity in cart');
-
-            }
-            this.persist();
-
-
-
-            // this.addProduct(product, quantity)
-            // const existingBusiness = this.businesses.find((item: any) => item.id == product.businesses_id)
-            // if (existingBusiness) {
-            //     const index = this.businesses.findIndex((item: any) => item.id == existingBusiness.id)
-            //     if (index > -1) {
-            //         this.businesses[index].cartItems.push({ product, quantity })
-            //     }
-            // } else {
-            //     const allBusinesses = businessStore.businesses
-            //     const business = allBusinesses?.find(business => business.id == product.businesses_id)
-            //     this.businesses.push({ id: product.businesses_id, business, cartItems: [{ product, quantity }] })
-            // }
-            return this;
-        },
-
-        removeAtIndex(index: number) {
-            this.items.splice(index, 1);
-
-            const toastStore = useToastStore();
-            toastStore.showSuccess('Removed From Cart');
-
-            this.persist();
-            return this;
-        },
-
-        removeProduct(product: Product) {
-            const index = this.items.findIndex(el => el.product.id == product.id);
-
-            if (index > -1) {
-                return this.removeAtIndex(index);
-            }
-
-            return this;
-        },
-
-        updateQuantity(product: Product, quantity: number = 0) {
-            // if (!this.hasProduct(product)) {
-            //     return;
-            // }
-
-            // const item = this.getProductItem(product);
-
-            // if (item) {
-            //     item.quantity = quantity ? quantity : item.quantity++;
-            //     this.persist();
-            // }
-
-            // return this;
-        },
-
-        async persist() {
-            await storage.set(KOLA_CART, this.orders, 30, 'days');
+        if (p) {
+          existingProduct = p;
+          return false;
         }
-    }
+      });
+
+      return existingProduct;
+    },
+
+    hasProduct(product: Product): boolean {
+      if (!product || !this.orders) {
+        return false;
+      }
+
+      let exists = false;
+
+      this.orders.forEach((order: Order) => {
+        const index = order.order_items.findIndex(
+          (el) => (el.products_id = product.id)
+        );
+        if (index > -1) {
+          exists = true;
+          return false;
+        }
+      });
+
+      return exists;
+    },
+
+    // addProduct(product: Product, quantity = 1) {
+    //     const toastStore = useToastStore();
+
+    //     if (this.hasProduct(product)) {
+    //         this.updateQuantity(product);
+    //         toastStore.showInfo('Increased quantity in cart');
+    //     } else {
+    //         this.items.push({ product, quantity })
+    //         toastStore.showSuccess('Added To Cart');
+    //     }
+
+    //     this.persist();
+    //     return this;
+    // },
+
+    getTotalCost() {
+      return this.orders.reduce((acc, value) => acc + value.getTotal(), 0);
+    },
+
+    addProduct(product: Product, quantity = 1) {
+      const toastStore = useToastStore();
+      const userStore = useUserStore();
+
+      let order = this.orders.find(
+        (order) => order.businesses_id == product.businesses_id
+      );
+
+      if (!order) {
+        order = new Order({
+          businesses_id: product.businesses_id,
+          customer_id: userStore.activeBusiness?.id,
+          cms_users_id: userStore.user?.id,
+          business: product.business,
+        });
+
+        this.orders.push(order);
+      }
+
+      let orderItem = order.order_items.find(
+        (item: OrderItem) => item.products_id == product.id
+      );
+
+      if (!orderItem) {
+        orderItem = new OrderItem({
+          businesses_id: product.businesses_id,
+          products_id: product.id,
+          product_image: product.image,
+          unit_price: product.product_price,
+          quantity: quantity,
+          total_price: quantity * (product.product_price || 0),
+          currencies_id: 1, // GHS
+          product_units_id: 1, // GHS
+        });
+
+        order.order_items.push(orderItem);
+
+        toastStore.showSuccess("Added To Cart");
+      } else {
+        orderItem.quantity = quantity;
+        toastStore.showInfo("Increased quantity in cart");
+      }
+      this.persist();
+
+      // this.addProduct(product, quantity)
+      // const existingBusiness = this.businesses.find((item: any) => item.id == product.businesses_id)
+      // if (existingBusiness) {
+      //     const index = this.businesses.findIndex((item: any) => item.id == existingBusiness.id)
+      //     if (index > -1) {
+      //         this.businesses[index].cartItems.push({ product, quantity })
+      //     }
+      // } else {
+      //     const allBusinesses = businessStore.businesses
+      //     const business = allBusinesses?.find(business => business.id == product.businesses_id)
+      //     this.businesses.push({ id: product.businesses_id, business, cartItems: [{ product, quantity }] })
+      // }
+      return this;
+    },
+
+    removeAtIndex(index: number) {
+      this.items.splice(index, 1);
+
+      const toastStore = useToastStore();
+      toastStore.showSuccess("Removed From Cart");
+
+      this.persist();
+      return this;
+    },
+
+    removeProduct(product: Product) {
+      const index = this.items.findIndex((el) => el.product.id == product.id);
+
+      if (index > -1) {
+        return this.removeAtIndex(index);
+      }
+
+      return this;
+    },
+
+    updateQuantity(product: Product, quantity: number = 0) {
+      // if (!this.hasProduct(product)) {
+      //     return;
+      // }
+      // const item = this.getProductItem(product);
+      // if (item) {
+      //     item.quantity = quantity ? quantity : item.quantity++;
+      //     this.persist();
+      // }
+      // return this;
+    },
+
+    async persist() {
+      await storage.set(KOLA_CART, this.orders, 30, "days");
+    },
+  },
 });
