4370ec06a8538d27ffb46dc7d57fce24c29b4df0
hagan199 <haganemmanuel23@gmail.com>
2023-10-31 20:49:32 +0000

    Update PaymentOptions.vue

--- src/views/Shopper/PaymentOptions.vue
+++ src/views/Shopper/PaymentOptions.vue
@@ -8,46 +8,77 @@
 
     <!-- Main Content -->
     <ion-content :fullscreen="true" class="ion-padding-horizontal">
-
-      <ion-card-header>
-        <ion-card-title class="title">Pay now</ion-card-title>
-      </ion-card-header>
-      <ion-card-content>
-        <ion-text class="description">Select a payment option</ion-text>
-      </ion-card-content>
-
-      <ion-card>
+      <section>
         <ion-card-header>
-          <ion-card-subtitle>Pay Now</ion-card-subtitle>
+          <ion-card-title class="title">Pay now</ion-card-title>
         </ion-card-header>
         <ion-card-content>
-          <span> 1% Discount per box</span>
-          <span> Guaranteed 3-Day delivery</span>
+          <ion-text class="description">Select a payment option</ion-text>
         </ion-card-content>
-      </ion-card>
+      </section>
 
+      <section>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>Pay Now</ion-card-subtitle>
+          </ion-card-header>
+          <ion-card-content>
+            <span> 1% Discount per box</span>
 
-      <ion-card>
+            <ion-icon name="time-outline"></ion-icon>
+            <span> Guaranteed 3-Day delivery</span>
+          </ion-card-content>
+
+        </ion-card>
+      </section>
+
+      <section>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>Pay on Delivery</ion-card-subtitle>
+          </ion-card-header>
+          <ion-card-content>
+            Standard Pricing
+          </ion-card-content>
+        </ion-card>
+      </section>
+      <section>
         <ion-card-header>
-          <ion-card-title>Card Title</ion-card-title>
-          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          <ion-card-title class="title">Pay later</ion-card-title>
         </ion-card-header>
         <ion-card-content>
-          Here's a small text description for the card content. Nothing more, nothing less.
+          <ion-text class="description">Select a pay later option</ion-text>
         </ion-card-content>
-      </ion-card>
+      </section>
 
-      <!-- Express Delivery Card -->
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Express Delivery</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>
-          Want to speed up delivery and receive your order today? We can do that for you
-        </ion-card-content>
-      </ion-card>
+      <ion-card class="main-container">
+    <ion-card-header class="pay-text">
+      Pay over 2 weeks
+    </ion-card-header>
+    <ion-card-content class="image-wrapper">
+      <ion-img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4445a350-54e6-4894-981e-29e9d0e66613?apiKey=76d19367cfa34bd4a80bc8549db79d6e&"
+        class="img"
+      ></ion-img>
+    </ion-card-content>
+  </ion-card>
+
+  
+  <ion-card class="main-container">
+    <ion-card-header class="pay-text">
+      Pay over 4 weeks
+    </ion-card-header>
+    <ion-card-content class="image-wrapper">
+      <ion-img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4445a350-54e6-4894-981e-29e9d0e66613?apiKey=76d19367cfa34bd4a80bc8549db79d6e&"
+        class="img"
+      ></ion-img>
+    </ion-card-content>
+  </ion-card>
+
     </ion-content>
-
     <!-- Footer -->
     <IonFooter class="ion-padding ion-no-border">
       <KolaYellowButton>Continue</KolaYellowButton>
@@ -63,7 +94,7 @@ import {
   IonSearchbar, IonSkeletonText, IonTitle, IonToolbar, IonText, IonInput, IonImg
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { close, heartOutline, heart, cart, cartOutline, shareOutline, navigateOutline } from 'ionicons/icons';
+import { close, heartOutline, heart, cart, cartOutline, shareOutline } from 'ionicons/icons';
 import KolaYellowButton from '@/components/KolaYellowButton.vue';
 import { mapStores } from 'pinia';
 import { useProductStore } from '@/stores/ProductStore';
@@ -137,4 +168,36 @@ export default defineComponent({
 </script>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main-container {
+  border-radius: 8px;
+  box-shadow: 0px 4px 24px 0px rgba(105, 111, 130, 0.1);
+  background-color: #fff;
+  width: 358px;
+  padding: 0;
+  margin: 16px 0;
+}
+
+.pay-text {
+  color: var(--ion-text-primary, #000);
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 157%;
+  font-family: 'Poppins', sans-serif;
+  text-align: center;
+  padding: 16px 0;
+}
+
+.image-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+}
+
+.img {
+  max-width: 100%;
+  object-fit: contain;
+}
+
+</style>
