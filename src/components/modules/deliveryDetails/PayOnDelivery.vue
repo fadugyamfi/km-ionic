c6ca83534127b4dbecde43c03638bef6e4b3a762
hagan199 <haganemmanuel23@gmail.com>
2023-11-07 23:19:28 +0000

    Fix the ion radio button

--- src/components/modules/deliveryDetails/PayOnDelivery.vue
+++ src/components/modules/deliveryDetails/PayOnDelivery.vue
@@ -1,35 +1,36 @@
 <template>
-  <section class="wrapper ion-padding ion-margin-bottom">
-    <ion-radio-group v-model="selectedOptionPayNow">
-      <section class="d-flex ion-justify-content-between ion-align-items-center" style="margin-bottom: 8px">
-        <IonText class="fw-semibold">Pay Now</IonText>
-        <section class="d-flex ion-align-items-center">
-          <IonText class="fw-semibold ion-margin-end"></IonText>
-          <ion-radio value="payNow"></ion-radio>
+  <section>
+    <ion-radio-group v-model="seletedPaymentOption">
+    <ion-card>
+        <section class="d-flex ion-justify-content-between ion-align-items-center" style="margin-bottom: 8px">
+          <IonText class="fw-semibold">Pay Now</IonText>
+          <section class="d-flex ion-align-items-center">
+            <IonText class="fw-semibold ion-margin-end"></IonText>
+            <ion-radio value="1"></ion-radio>
+          </section>
         </section>
-      </section>
-      <section class="d-flex flex-column">
-        <IonText color="medium" class="font-medium" style="margin-bottom: 8px">
-          <IonIcon :icon="locationOutline" style="margin-right: 3px"></IonIcon>1% Discount Per Box
-        </IonText>
-        <IonText color="medium" class="font-medium" style="margin-bottom: 8px">
-          <IonIcon :icon="timeOutline" style="margin-right: 3px"></IonIcon>Guaranteed 3-Day Delivery
-        </IonText>
-      </section>
-    </ion-radio-group>
-  </section>
-  <section class="wrapper ion-padding">
-    <ion-radio-group v-model="selectedOptionPayOnDelivery">
-      <section class="d-flex ion-justify-content-between ion-align-items-center" style="margin-bottom: 8px">
-        <IonText class="fw-semibold">Pay on Delivery</IonText>
-        <section class="d-flex ion-align-items-center">
-          <ion-radio value="payOnDelivery"></ion-radio>
+        <section class="d-flex flex-column">
+          <IonText color="medium" class="font-medium" style="margin-bottom: 8px">
+            <IonIcon :icon="locationOutline" style="margin-right: 3px"></IonIcon>1% Discount Per Box
+          </IonText>
+          <IonText color="medium" class="font-medium" style="margin-bottom: 8px">
+            <IonIcon :icon="timeOutline" style="margin-right: 3px"></IonIcon>Guaranteed 3-Day Delivery
+          </IonText>
         </section>
-      </section>
-      <section class="d-flex flex-column">
-        <IonText color="medium" class="font-medium" style="margin-bottom: 8px">Standard Pricing</IonText>
-      </section>
-    </ion-radio-group>
+    </ion-card>
+
+    <ion-card>
+        <section class="d-flex ion-justify-content-between ion-align-items-center" style="margin-bottom: 8px">
+          <IonText class="fw-semibold">Pay on Delivery</IonText>
+          <section class="d-flex ion-align-items-center">
+            <ion-radio value="2"></ion-radio>
+          </section>
+        </section>
+        <section class="d-flex flex-column">
+          <IonText color="medium" class="font-medium" style="margin-bottom: 8px">Standard Pricing</IonText>
+        </section>
+    </ion-card>
+  </ion-radio-group>
   </section>
 </template>
 
@@ -37,53 +38,37 @@
 import { locationOutline, timeOutline } from "ionicons/icons";
 import { IonText, IonRadioGroup } from "@ionic/vue";
 import { ref, watch } from "vue";
+const seletedPaymentOption = ref(null);
 
-const selectedOptionPayNow = ref(null);
-const selectedOptionPayOnDelivery = ref(null);
-
-watch(selectedOptionPayNow, (newValue) => {
-  if (newValue === "payNow") {
-    selectedOptionPayOnDelivery.value = null; 
-  }
-});
-
-watch(selectedOptionPayOnDelivery, (newValue) => {
-  if (newValue === "payOnDelivery") {
-    selectedOptionPayNow.value = null; 
-  }
-});
 </script>
 
 <style scoped>
-
+ion-card {
+  margin: 6px;
+  padding: 9px;
+}
 ion-radio::part(container) {
   width: 18px;
   height: 18px;
-
   border-radius: 20px;
   border: 1.5px solid #e7eaec;
 }
-
 ion-radio::part(mark) {
   background: none;
   transition: none;
   transform: none;
   border-radius: 0;
 }
-
 ion-radio.radio-checked::part(container) {
   background: #21d187;
   border-color: transparent;
 }
-
 ion-radio.radio-checked::part(mark) {
   width: 6px;
   height: 9px;
-
   border-width: 0px 2px 2px 0px;
   border-style: solid;
   border-color: #fff;
-
   transform: rotate(45deg);
 }
 </style>
--- src/views/Shopper/Cart/PaymentOptions.vue
+++ src/views/Shopper/Cart/PaymentOptions.vue
@@ -10,9 +10,7 @@
       <form>
         <section class="d-flex flex-column ion-margin-bottom">
           <IonText class="fw-semibold">Pay Now</IonText>
-          <IonText color="medium" class="font-medium"
-            >Select a payment method</IonText
-          >
+          <IonText color="medium" class="font-medium">Select a payment method</IonText>
         </section>
 
         <section class="d-flex flex-column ion-margin-bottom">
@@ -21,127 +19,100 @@
 
         <section class="d-flex flex-column ion-margin-bottom">
           <IonText class="fw-semibold">Pay Later</IonText>
-          <IonText color="medium" class="font-medium"
-            >Select a pay later option</IonText
-          >
+          <IonText color="medium" class="font-medium">Select a pay later option</IonText>
         </section>
-
-        <section class="d-flex flex-column ion-margin-bottom">
-          <!-----------Pay over 2 weeks----------------->
-          <ion-card>
-            <ion-card-content>
-              <section
-                class="d-flex ion-justify-content-between ion-align-items-center"
-              >
-                <IonText class="fw-semibold">Pay over 2 weeks</IonText>
-                <section class="d-flex ion-align-items-center">
-                  <img
-                    loading="lazy"
-                    src="/img/icons/chevron-down.svg"
-                    class="image"
-                    @click="toggleDropdown('pay2Weeks')"
-                  />
+        <ion-radio-group v-model="form.fields.payment_option">
+          <section class="d-flex flex-column ion-margin-bottom">
+            <!-----------Pay over 2 weeks----------------->
+            <ion-card>
+              <ion-card-content @click="toggleDropdown('pay2Weeks')">
+                <section class="d-flex ion-justify-content-between ion-align-items-center">
+                  <IonText class="fw-semibold">Pay over 2 weeks</IonText>
+                  <section class="d-flex ion-align-items-center">
+                    <img loading="lazy" src="/img/icons/chevron-down.svg" class="image" />
+                  </section>
                 </section>
-              </section>
-            </ion-card-content>
-          </ion-card>
-          <ion-card v-if="showDropdown.pay2Weeks">
-            <section class="card-section">
-              <ion-radio-group>
+              </ion-card-content>
+            </ion-card>
+            <ion-card v-if="showDropdown.pay2Weeks">
+              <section class="card-section">
+
                 <section class="radio-section">
                   <p class="radio-text">Pay 50% installment each week</p>
                   <section class="d-flex ion-align-items-center">
                     <IonText class="fw-semibold ion-margin-end"></IonText>
-                    <ion-radio
-                      v-model="form.fields.payLaterDueDate"
-                      :disabled="form.fields.payLaterInstallmentWeeks"
-                    ></ion-radio>
+                    <ion-radio value="3"></ion-radio>
                   </section>
                 </section>
-              </ion-radio-group>
-              <ion-radio-group>
                 <section class="radio-section">
                   <p class="radio-text">Pay 100% on the due date</p>
                   <section class="d-flex ion-align-items-center">
                     <IonText class="fw-semibold ion-margin-end"></IonText>
-                    <ion-radio
-                      v-model="form.fields.payLaterInstallmentWeeks"
-                      :disabled="form.fields.payLaterDueDate"
-                    ></ion-radio>
+                    <ion-radio value="4"></ion-radio>
                   </section>
                 </section>
-              </ion-radio-group>
-              <p class="radio-text">
-                Subject to Approval. Markup of 2.5% will be added to the total
-                price
-              </p>
-            </section>
-          </ion-card>
 
-          <!-----------Pay over 4 weeks----------------->
-          <ion-card>
-            <ion-card-content>
-              <section
-                class="d-flex ion-justify-content-between ion-align-items-center"
-              >
-                <IonText class="fw-semibold">Pay over 4 weeks</IonText>
-                <section class="d-flex ion-align-items-center">
-                  <img
-                    loading="lazy"
-                    src="/img/icons/chevron-down.svg"
-                    class="image"
-                    @click="toggleDropdown('pay4Weeks')"
-                  />
-                </section>
+                <p class="radio-text">
+                  Subject to Approval. Markup of 2.5% will be added to the total
+                  price
+                </p>
               </section>
-            </ion-card-content>
-          </ion-card>
-          <ion-card v-if="showDropdown.pay4Weeks">
-            <section class="card-section">
-              <ion-radio-group>
+            </ion-card>
+            <!-----------Pay over 4 weeks----------------->
+            <ion-card>
+              <ion-card-content @click="toggleDropdown('pay4Weeks')">
+                <section class="d-flex ion-justify-content-between ion-align-items-center">
+                  <IonText class="fw-semibold">Pay over 4 weeks</IonText>
+                  <section class="d-flex ion-align-items-center">
+                    <img loading="lazy" src="/img/icons/chevron-down.svg" class="image" />
+                  </section>
+                </section>
+              </ion-card-content>
+            </ion-card>
+            <ion-card v-if="showDropdown.pay4Weeks">
+              <section class="card-section">
+
                 <section class="radio-section">
                   <p class="radio-text">Pay 25% instalment each week</p>
                   <section class="d-flex ion-align-items-center">
                     <IonText class="fw-semibold ion-margin-end"></IonText>
-                    <ion-radio
-                      v-model="form.fields.payInstalmentWeek"
-                    ></ion-radio>
+                    <ion-radio value="5"></ion-radio>
                   </section>
                 </section>
-              </ion-radio-group>
-              <ion-radio-group>
+
                 <section class="radio-section">
                   <p class="radio-text">Pay 50% instalment every 2 weeks</p>
                   <section class="d-flex ion-align-items-center">
                     <IonText class="fw-semibold ion-margin-end"></IonText>
-                    <ion-radio v-model="form.fields.payInstalment"></ion-radio>
+                    <ion-radio value="6"></ion-radio>
                   </section>
                 </section>
-              </ion-radio-group>
-              <ion-radio-group>
+
                 <section class="radio-section">
                   <p class="radio-text">Pay 100% on the due date</p>
                   <section class="d-flex ion-align-items-center">
                     <IonText class="fw-semibold ion-margin-end"></IonText>
-                    <ion-radio v-model="form.fields.payDueDate"></ion-radio>
+                    <ion-radio value="7"></ion-radio>
                   </section>
                 </section>
-              </ion-radio-group>
-              <p class="radio-text">
-                Subject to Approval. Markup of 2.5% will be added to the total
-                price
-              </p>
-            </section>
-          </ion-card>
-        </section>
 
-        <IonFooter class="ion-padding ion-no-border">
-          <KolaYellowButton @click="viewItemReview">Continue</KolaYellowButton>
-        </IonFooter>
+                <p class="radio-text">
+                  Subject to Approval. Markup of 2.5% will be added to the total
+                  price
+                </p>
+              </section>
+            </ion-card>
+          </section>
+        </ion-radio-group>
       </form>
     </ion-content>
+    <IonFooter class="ion-padding ion-no-border">
+      <KolaYellowButton @click="storePaymentOption">Continue</KolaYellowButton>
+    </IonFooter>
   </ion-page>
 </template>
+
+
 
 <script lang="ts">
 import {
@@ -183,14 +154,13 @@ import {
   shareOutline,
 } from "ionicons/icons";
 import KolaYellowButton from "@/components/KolaYellowButton.vue";
-import { mapStores } from "pinia";
-import { useProductStore } from "@/stores/ProductStore";
 import KolaWhiteButton from "@/components/KolaWhiteButton.vue";
 import Image from "@/components/Image.vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import NoResults from "../../../components/layout/NoResults.vue";
 import PaymentOptionsHeader from "@/components/header/PaymentOptionsHeader.vue";
 import PayOnDelivery from "@/components/modules/deliveryDetails/PayOnDelivery.vue";
+import { useCartStore } from "@/stores/CartStore";
 
 type DropdownName = "pay2Weeks" | "pay4Weeks";
 
@@ -249,37 +219,27 @@ export default defineComponent({
       showPayDropdown: false,
       form: {
         fields: {
-          payLaterInstallmentWeeks: false,
-          payLaterDueDate: false,
-          payInstalment: false,
-          payInstalmentWeek: false,
-          payDueDate: false,
+          payment_option: '',
         },
       },
     };
   },
-
-  // ...mapStores(useProductStore)
-  handleRadioSelection(selectedOption: string) {
-    if (selectedOption === "payLaterInstallmentWeeks") {
-      this.form.fields.payLaterDueDate = false;
-      this.form.fields.payLaterInstallmentWeeks = true; // Enable installment radio
-    } else if (selectedOption === "payLaterDueDate") {
-      this.form.fields.payLaterInstallmentWeeks = false;
-      this.form.fields.payLaterDueDate = true; // Enable due date radio
-    }
-  },
-
+  
   methods: {
-    handleRadioSelection(selectedOption: string) {
-      if (selectedOption === "payLaterInstallmentWeeks") {
-        this.form.fields.payLaterDueDate = false;
-        this.form.fields.payLaterInstallmentWeeks = true;
-      } else if (selectedOption === "payLaterDueDate") {
-        this.form.fields.payLaterInstallmentWeeks = false;
-        this.form.fields.payLaterDueDate = true;
-      }
+
+    storePaymentOption() {
+      const cartStore = useCartStore();
+      const index = cartStore.orders.findIndex(
+        (b) => b.businesses_id == Number(this.$route.params.id)
+      );
+      cartStore.orders[index] = {
+        ...cartStore.orders[index],
+        payment_option: this.form.fields.payment_option,
+        
+      };
+      this.$router.push(`/shopper/cart/business/${this.$route.params.id}/item-review`);
     },
+
     viewItemReview() {
       this.$router.push(
         `/shopper/cart/business/${this.$route.params.id}/item-review`
