b210a2fee05182ce9b9ae17d9b084b3c17d9e93b
frankassafuah <frankassafuah@gmail.com>
2023-11-19 18:10:39 +0000

    agent sales report ui

---
+++ src/components/modules/agents/BestSellingItems.vue
@@ -0,0 +1,130 @@
+<template>
+  <IonRow>
+    <IonCol size="6">
+      <IonCard class="ion-no-margin best-selling-card">
+        <IonCardContent class="d-flex flex-column">
+          <IonText color="medium">Best Selling Item</IonText>
+          <IonText color="dark" class="fw-semibold"> N/A </IonText>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+    <IonCol size="6">
+      <IonCard class="ion-no-margin customers-card">
+        <IonCardContent class="d-flex flex-column">
+          <IonText color="medium">No. of Customers</IonText>
+          <IonText color="dark" class="fw-semibold">
+            <!-- {{ Filters.number(businessStore.businessSummary?.customer_count) }} -->
+            N/A
+          </IonText>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+  </IonRow>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonIcon,
+  IonCardContent,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonItem,
+  IonText,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { mapStores } from "pinia";
+import { add, arrowUpOutline, optionsOutline, close } from "ionicons/icons";
+import { useBusinessStore } from "../../../stores/BusinessStore";
+import { useUserStore } from "../../../stores/UserStore";
+import Business from "../../../models/Business";
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardHeader,
+    IonIcon,
+    IonCardContent,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonItem,
+    IonText,
+  },
+
+  data() {
+    return {
+      add,
+      optionsOutline,
+      arrowUpOutline,
+      close,
+      showFilterSheet: false,
+      //   recentSales: [] as Sale[],
+      fetchingSummary: false,
+    };
+  },
+
+  //   computed: {
+  //     ...mapStores(useSaleStore, useBusinessStore, useUserStore),
+
+  //     percentageOfConversion() {
+  //       const orders = this.businessStore.businessSummary?.orders;
+  //       return (orders?.converted_to_sales_count / orders?.total_count) * 100;
+  //     },
+
+  //     percentageComplete() {
+  //       return this.businessStore.businessSummary?.targets?.percentage_complete;
+  //     },
+  //   },
+
+  //   methods: {
+  //     onAddSale() {
+  //       this.saleStore.resetForNewSale();
+  //       this.$router.push("/vendor/sales/add-sale/select-agent");
+  //     },
+
+  //     onViewSales() {
+  //       this.$router.push("/vendor/sales/history");
+  //     },
+
+  //     fetchRecentSales() {
+  //       this.saleStore.fetchSales({ limit: 5, sort: "latest" });
+  //     },
+
+  //     async fetchBusinessSummary() {
+  //       this.fetchingSummary = true;
+  //       await this.businessStore.getBusinessSummary(
+  //         this.userStore.activeBusiness as Business
+  //       );
+  //       this.fetchingSummary = false;
+  //     },
+  //   },
+
+  //   mounted() {
+  //     this.fetchRecentSales();
+  //     this.fetchBusinessSummary();
+  //   },
+});
+</script>
+
+<style scoped>
+.customers-card {
+  --background: #f2f7f7;
+}
+
+.best-selling-card {
+  --background: #fff2ebcc;
+}
+</style>
---
+++ src/views/Profile/SaleAgents/AgentSalesReport.vue
@@ -0,0 +1,164 @@
+<template>
+  <ion-page>
+    <IonHeader class="ion-padding ion-no-border">
+      <ion-header class="inner-header">
+        <ion-toolbar class="ion-align-items-center">
+          <ion-buttons slot="start">
+            <ion-back-button
+              color="dark"
+              :icon="arrowBackOutline"
+              text=""
+              style="margin-left: 10px"
+              defaultHref="/profile/company/sale-agents"
+            ></ion-back-button>
+          </ion-buttons>
+          <IonTitle size="small" class="fw-bold">
+            <section
+              class="d-flex ion-align-items-center ion-justify-content-center"
+            >
+              <IonLabel>{{ $t("Agent sales report") }}</IonLabel>
+            </section></IonTitle
+          >
+        </ion-toolbar>
+      </ion-header>
+      <IonToolbar>
+        <IonSegment
+          value="all"
+          mode="ios"
+          @ionChange="onSegmentChanged($event)"
+        >
+          <IonSegmentButton value="all">
+            <IonLabel> Today </IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="request">
+            <IonLabel>This week</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="leaderboard">
+            <IonLabel>This month</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+      </IonToolbar>
+    </IonHeader>
+    <ion-content>
+      <SalesStatistics></SalesStatistics>
+      <Performance></Performance>
+      <BestSellingItems></BestSellingItems>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  IonSpinner,
+  IonTitle,
+  RefresherCustomEvent,
+  IonRefresherContent,
+  IonRefresher,
+  IonLabel,
+  IonIcon,
+  IonBadge,
+  IonSearchbar,
+  IonList,
+  IonSegmentButton,
+  IonSegment,
+} from "@ionic/vue";
+import { computed, onBeforeUnmount, onMounted, ref } from "vue";
+import { arrowBackOutline, personAddOutline, search } from "ionicons/icons";
+import { useUserStore } from "@/stores/UserStore";
+import { useBusinessStore } from "@/stores/BusinessStore";
+import Business from "@/models/Business";
+import User from "@/models/User";
+import { useRouter } from "vue-router";
+import { useAgentsStore } from "@/stores/AgentsStore";
+import SalesStatistics from "../../../components/modules/agents/SalesStatistics.vue";
+import Performance from "../../../components/modules/agents/Performance.vue";
+import BestSellingItems from "../../../components/modules/agents/BestSellingItems.vue";
+
+const fetching = ref(false);
+const refreshing = ref(false);
+const router = useRouter();
+const searchEnabled = ref(false);
+const agents = ref<User[]>([]);
+
+const handleRefresh = async (event: RefresherCustomEvent) => {
+  refreshing.value = true;
+  fetching.value = true;
+  await fetchAgents();
+  refreshing.value = false;
+  event.target.complete();
+};
+
+const onSearch = async (event: Event) => {
+  refreshing.value = true;
+  fetching.value = true;
+  await fetchAgents({
+    name_like: (event.target as HTMLIonSearchbarElement).value,
+  });
+  refreshing.value = false;
+};
+const onSegmentChanged = (event: CustomEvent) => {
+  let start_dt = new Date();
+  let end_dt = new Date();
+  const option = event.detail.value;
+
+  switch (option) {
+    case "all":
+      start_dt.setMonth(start_dt.getMonth() - 1);
+      break;
+
+    case "request":
+      start_dt.setDate(start_dt.getDate() - 1);
+      break;
+
+    case "leaderboard":
+      start_dt.setDate(start_dt.getDate() - 7);
+      break;
+  }
+
+  //   this.searchFilters.start_dt = formatMySQLDateTime(start_dt.toISOString());
+  //   this.searchFilters.end_dt = formatMySQLDateTime(end_dt.toISOString());
+
+  //   this.fetchOrders();
+};
+const fetchAgents = async (options = {}) => {
+  if (agents.value?.length == 0) {
+    fetching.value = true;
+  }
+  const userStore = useUserStore();
+  const agentsStore = useAgentsStore();
+
+  agents.value = await agentsStore.getBusinessSaleAgents(
+    userStore.activeBusiness as Business,
+    100,
+    options,
+    refreshing.value
+  );
+  fetching.value = false;
+};
+
+onMounted(() => {
+  fetchAgents();
+  //   this.onSegmentChanged(new CustomEvent('load', { detail: { value: 'thisweek' } }));
+});
+</script>
+
+<style lang="scss" scoped>
+ion-badge.badge {
+  --background: rgba(245, 170, 41, 0.38);
+  --color: #344054;
+  margin-left: 8px;
+}
+ion-segment {
+  ion-segment-button {
+    padding-top: 0.4em;
+    padding-bottom: 0.4em;
+  }
+}
+</style>
