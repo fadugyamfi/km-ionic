c442c07b471d6c79a03033ed50829bd98cfa0c52
frankassafuah <frankassafuah@gmail.com>
2024-04-12 10:28:20 +0000

    add statistics to vendor dashboard

--- src/views/Vendor/Home/Home.vue
+++ src/views/Vendor/Home/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <ion-page>
-
     <ShopperHeader></ShopperHeader>
 
     <ion-content :fullscreen="true">
       <VendorHeader></VendorHeader>
 
-      <OrderListNotification></OrderListNotification>
+      <OrderListNotification
+        :orders="orderStore?.orders"
+      ></OrderListNotification>
+
+      <!-- <OrderStatistics
+        :orderSummary="businessStore?.businessSummary?.orders"
+        :no-of-new-orders="orderStore?.orders?.length"
+      ></OrderStatistics> -->
+
+      <!-- <SalesStatistics
+        :total-sales="businessStore?.businessSummary?.total_sales_value"
+        :avg-sales="businessStore?.businessSummary?.average_sales_value"
+        :total-order-value="businessStore?.businessSummary?.orders?.total_value"
+        showTotalOrderValue
+      ></SalesStatistics> -->
 
       <GrowYourStoreInfo></GrowYourStoreInfo>
 
@@ -16,12 +29,56 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent } from '@ionic/vue';
-import ShopperHeader from '@/components/layout/ShopperHeader.vue';
-import ModeToggleCard from '@/components/cards/ModeToggleCard.vue';
-import VendorHeader from './VendorHeader.vue';
-import OrderListNotification from './OrderListNotification.vue';
-import GrowYourStoreInfo from './GrowYourStoreInfo.vue';
+import { IonPage, IonContent } from "@ionic/vue";
+import ShopperHeader from "@/components/layout/ShopperHeader.vue";
+import ModeToggleCard from "@/components/cards/ModeToggleCard.vue";
+import VendorHeader from "./VendorHeader.vue";
+import OrderListNotification from "./OrderListNotification.vue";
+import GrowYourStoreInfo from "./GrowYourStoreInfo.vue";
+import OrderStatistics from "./OrderStatistics.vue";
+import SalesStatistics from "@/components/modules/SalesStatistics.vue";
+import { onMounted, ref } from "vue";
+import { formatMySQLDateTime, handleAxiosRequestError } from "@/utilities";
+import { useOrderStore } from "@/stores/OrderStore";
+import { useBusinessStore } from "@/stores/BusinessStore";
+import Business from "@/models/Business";
+import { useUserStore } from "@/stores/UserStore";
+import { Order } from "@/models/Order";
+
+const orderStore = useOrderStore();
+const userStore = useUserStore();
+const businessStore = useBusinessStore();
+
+const fetching = ref(false);
+
+const fetchOrders = async () => {
+  try {
+    fetching.value = true;
+    let start_dt = new Date();
+    let end_dt = formatMySQLDateTime(new Date().toISOString());
+
+    start_dt.setDate(start_dt.getDate() - 7);
+
+    await orderStore.fetchReceivedOrders({
+      start_dt: formatMySQLDateTime(start_dt.toISOString()),
+      end_dt,
+    });
+  } catch (error) {
+    handleAxiosRequestError(error);
+  } finally {
+    fetching.value = false;
+  }
+};
+const fetchBusinessSummary = async () => {
+  fetching.value = true;
+  await businessStore.getBusinessSummary(userStore.activeBusiness as Business);
+  fetching.value = false;
+};
+
+onMounted(() => {
+  fetchOrders();
+  fetchBusinessSummary();
+});
 </script>
 
 <style lang="scss">
---
+++ src/views/Vendor/Home/OrderStatistics.vue
@@ -0,0 +1,59 @@
+<template>
+  <IonRow>
+    <IonCol size="6">
+      <IonCard class="ion-no-margin">
+        <IonCardContent class="d-flex flex-column">
+          <IonText color="medium">{{
+            $t("general.totalOrdersReceived")
+          }}</IonText>
+          <IonText color="dark" class="fw-semibold">
+            {{ orderSummary?.total_count }}
+          </IonText>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+    <IonCol size="6">
+      <IonCard class="ion-no-margin">
+        <IonCardContent class="d-flex flex-column">
+          <IonText color="medium">{{ $t("general.newOrders") }}</IonText>
+          <IonText color="dark" class="fw-semibold">
+            {{ noOfNewOrders }}
+          </IonText>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+  </IonRow>
+</template>
+
+<script lang="ts">
+import { IonRow, IonCol, IonCard, IonCardContent, IonText } from "@ionic/vue";
+import Filters from "@/utilities/Filters";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  components: {
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    IonText,
+  },
+  props: {
+    orderSummary: {
+      type: Object,
+      default: () => {},
+    },
+    noOfNewOrders: {
+      type: [String, Number],
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      Filters,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
