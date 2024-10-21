4363a0eb1ce72b1e38e4d9c52c33633d0b43603c
frankassafuah <frankassafuah@gmail.com>
2024-10-21 17:43:33 +0000

    add to home screen testing

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from "./router";
 // import './registerServiceWorker';
 // import "./setupPushNotification";
 // import "./messaging_init_in_sw";
+import "./register-service-worker"
 
 import { IonicVue } from "@ionic/vue";
 
